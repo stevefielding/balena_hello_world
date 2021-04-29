cbac8d9fde55a524e9d7c849af3e893bc3bda024
stevefielding <base2designs>
2021-04-29 10:49:43 -0700

    Created

---
+++ README.md
@@ -0,0 +1,5 @@
+# balena_hello_world
+
+Balena container for RPi.  
+Prints loop count every 10s
+
---
+++ src/hello.py
@@ -0,0 +1,6 @@
+import time
+loopCnt = 0
+while True:
+  loopCnt += 1
+  print("Loop # {}".format(loopCnt))
+  time.sleep(10)
